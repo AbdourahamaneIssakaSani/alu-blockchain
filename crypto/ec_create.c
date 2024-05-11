b496d833a3b9445ec91b348a6af8628d9fdb8b3a
Abissa <67793634+AbdourahamaneIssakaSani@users.noreply.github.com>
2024-05-11 11:43:53 +0200

    :robot: automated commit of ec_create.c

---
+++ crypto/ec_create.c
@@ -0,0 +1,22 @@
+#include "hblk_crypto.h"
+
+
+/**
+* ec_create - create a new EC key pair
+*
+* Return: pointer to the created EC key pair, or NULL upon failure
+*/
+EC_KEY *ec_create(void)
+{
+	EC_KEY *key = NULL;
+
+	key = EC_KEY_new_by_curve_name(EC_CURVE);
+	if (!key)
+		return (NULL);
+	if (!EC_KEY_generate_key(key))
+	{
+		EC_KEY_free(key);
+		return (NULL);
+	}
+	return (key);
+}
