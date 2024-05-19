799fb8dea794ec852cf473267fd97fd789de2b26
Abissa <67793634+AbdourahamaneIssakaSani@users.noreply.github.com>
2024-05-19 17:19:19 +0200

    validity

--- blockchain/v0.1/block_is_valid.c
+++ blockchain/v0.1/block_is_valid.c
@@ -1,6 +1,7 @@
 #include "blockchain.h"
 
 
+
 /**
 * block_is_valid - checks if a Block is valid
 *
@@ -12,7 +13,25 @@
 int block_is_valid(block_t const *block, block_t const *prev_block)
 {
 	uint8_t hash_buf[SHA256_DIGEST_LENGTH];
-	block_t const _genesis = GENESIS_BLOCK;
+
+	block_t genesis_block = {
+		{ /* info */
+			972, /* index */
+			98, /* difficulty */
+			1537578000, /* timestamp */
+			94104, /* nonce */
+			"\xdb\x74\xc9\x40\xd4\x47\xe8\x77\xd1\x19\xdf\x61\x3e\xdd\x27\x00\xc4"
+			"\xa8\x4c\xd1\xcf\x08\xbe\xb7\xcb\xc3\x19\xbc\xfa\xea\xb9\x7a" /* prev */
+		},
+		{ /* data */
+			"Holberton School", /* buffer */
+			16 /* len */
+		},
+		{0} /* hash */
+	};
+
+
+	block_t const _genesis = genesis_block;
 
 	block_t const *_prev_block = prev_block ? prev_block : &_genesis;
 
