342b43b40bf8e8105ec105bd16812bb4009f5fea
Abissa <67793634+AbdourahamaneIssakaSani@users.noreply.github.com>
2024-05-26 16:33:20 +0200

    :robot: automated commit of transaction/tx_in_create.c

--- blockchain/v0.3/transaction/tx_in_create.c
+++ blockchain/v0.3/transaction/tx_in_create.c
@@ -21,7 +21,8 @@ tx_in_t *tx_in_create(unspent_tx_out_t const *unspent)
 
 	memcpy(tx_in->block_hash, unspent->block_hash, sizeof(tx_in->block_hash));
 	memcpy(tx_in->tx_id, unspent->tx_id, sizeof(tx_in->tx_id));
-	memcpy(tx_in->tx_out_hash, &unspent->out, sizeof(tx_in->tx_out_hash));
+	memcpy(tx_in->tx_out_hash, unspent->out.hash, sizeof(tx_in->tx_out_hash));
+
 
 	return (tx_in);
 }
