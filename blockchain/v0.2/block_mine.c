685ad99aea70b7a76555a30a7e6ab8e8a33fd8f9
Abissa <67793634+AbdourahamaneIssakaSani@users.noreply.github.com>
2024-06-02 17:24:09 +0200

    test 2

--- blockchain/v0.2/block_mine.c
+++ blockchain/v0.2/block_mine.c
@@ -6,21 +6,23 @@
  */
 void block_mine(block_t *block)
 {
-	uint8_t buff[SHA256_DIGEST_LENGTH];
+	/* nonce is a 64-bit number that is typically used once */
+	uint64_t nonce = 0;
+	/* hash is a 256-bit number */
+	uint8_t hash[SHA256_DIGEST_LENGTH];
 
-	if ((!block) || (block->info.difficulty == 0))
-		return;
-
-	block_hash(block, block->hash);
-
-	for (;;)
+	while (1)
 	{
-		block->info.nonce++;
-		block_hash(block, buff);
-		if (hash_matches_difficulty(buff, block->info.difficulty))
-		{
-			memcpy(block->hash, buff, SHA256_DIGEST_LENGTH);
+		/* Update the block's info with the new nonce */
+		block->info.nonce = nonce;
+		/* Compute the hash of the block */
+		block_hash(block, hash);
+		/* Check if the hash matches the difficulty */
+		if (hash_matches_difficulty(hash, block->info.difficulty))
 			break;
-		}
+		/* Increment the nonce */
+		nonce++;
 	}
+	/* Copy the resulting hash to the block's data */
+	memcpy(block->hash, hash, SHA256_DIGEST_LENGTH);
 }
